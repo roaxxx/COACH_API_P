a34f648e3e81f1a2cd635e3b24737a3780e81596
roaxxx <102877897+roaxxx@users.noreply.github.com>
2022-12-13 16:03:12 -0500

    1.10

--- Classes/Login/Login.php
+++ Classes/Login/Login.php
@@ -1,21 +1,34 @@
 <?php
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         require_once "../Connection/Connection.php";
-        require_once "../Costcenters/CostCenter.php";
+        require_once "../Costcenters/Company.php";
 
         $con = new Connection;
 
         $poolCompany = $_POST['poolCompany'];
-        $table = $_POST['table'];
+        $action = $_POST['action'];
 
-        $query2 = $con->getDBParameters("SELECT u.usuario, nombre_empresa, pool_empresa, password, ip_servidor FROM EMPRESA e JOIN USUARIO u ON e.usuario = u.usuario and pool_empresa ='".$poolCompany."'");
-        $con2 = new Company($query2);
-        if($table==1){
-            echo $con2->getDataList("SELECT id,nombre FROM CENTRO_COSTO");
-        }else if($table==2){
-            echo $con2->getDataList("SELECT id_persona as id, nombre as name FROM persona where rol ='cliente'");
-        }else if($table==3){
-            echo $con2->getDataList("SELECT id_persona as id,nombre as name,usuario as user,password as passw FROM NOMINA JOIN PERSONA ON persona = id_persona AND ROL = 'Empleado' AND usuario != '' AND password != ''");
+        $query = $con->getDBParameters("SELECT u.usuario, nombre_empresa, pool_empresa, password, ip_servidor FROM EMPRESA e JOIN USUARIO u ON e.usuario = u.usuario and pool_empresa ='".$poolCompany."'");
+        $con2 = new Company($query);
+        switch($action){
+            case 0:
+                echo $con2->getDataList("SELECT id,nombre FROM CENTRO_COSTO");
+                break;
+            case 1:
+                echo $con2->getDataList("SELECT id_persona as id, nombre as name FROM persona where rol ='cliente' or rol ='Empleado'");
+                break;
+            case 2:
+                echo $con2->getDataList("SELECT id_persona as id,nombre as name,usuario as user,password as passw FROM NOMINA JOIN PERSONA ON persona = id_persona AND usuario != '' AND password != ''");
+                break;
+            case 3:
+                echo $con2->getDataList("SELECT rut, nombre FROM empresa");
+                break;
+            case 4:
+                echo $con2->getDataList("SELECT id_linea_producto as idLinea, nombre FROM linea_producto");
+                break;
+            case 5:
+                echo $con2->getDataList("SELECT id_producto,linea_producto,nombre,precio_venta1,porcentaje_iva,foto FROM producto");
+                break;
         }
     }
 ?>
---
+++ Classes/Products/Pedidos.php
@@ -0,0 +1,6 @@
+<?php
+"INSERT INTO linea_registro_transaccion_producto
+(id_linea_registro_transaccion_producto,cantidad,valor_bruto,tarifa_iva,valor_total,transaccion,usuario_insercion,fecha_insercion,consecutivo_gravado,id_producto,ubicacion)
+VALUES
+(0,1,2941.18,558.82,3500.00,'PE','1082779418',current_timestamp(),'PE-540',1,1)"
+?>
